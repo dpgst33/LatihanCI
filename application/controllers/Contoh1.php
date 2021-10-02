7a98e852857f5845e83192809b2e6ccfbb799cad
Dpgst3 <delila.pangesti@gmail.com>
2021-10-01 14:32:59 -1200

    Tugas Pert2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php 
+class Contoh1 extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Udin  <br> Saya tinggal di daerah Pamulang  <br>  Olah raga yang saya sukai adalah Bulutangkis"; 
+    } 
+}
